3df4587d4a439cb7dcb9e34314eefc6534d5c6da
Waleed Zafar <58841410+waleedzafar4578@users.noreply.github.com>
2025-08-06 17:00:02 +0500

    Basic Architecture is ready.

---
+++ app/main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+# Add CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    # allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# here adding custom middlewars
+
+
+@app.middleware("http")
+async def identify_the_user(request: Request, call_next):
+    user_ip = request.client.host
+    print("===========================")
+    print(user_ip, request.client.port)
+    print(await request.body())
+    response = await call_next(request)
+    return response
+
+# Register API
+# app.include_router()
